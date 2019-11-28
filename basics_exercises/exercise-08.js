8f67aaf04c1ea80c6ffdc4ecb9ec8016e3a088b8
Sal Tamay <sal.tamay@outlook.com>
2019-11-27 21:45:21 -0500

    Basics exercise-08

---
+++ basics_exercises/exercise-08.js
@@ -0,0 +1,13 @@
+// Will an error occur if you try to access an array element with an index that is greater than or equal to the length of the array? For example:
+
+/**
+ * var foo = ['a', 'b', 'c'];
+ * foo.length;  // returns 3
+ * foo[3];      // will this result in an error?
+ */
+
+// Answer: Yes it won't since that undefined is assigned to that element bt JS engine
+
+var foo = ['a', 'b', 'c'];
+foo.length;  // returns 3
+foo[3];      // will this result in an error?
