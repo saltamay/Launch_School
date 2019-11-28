e072ad1264b1080f40c3b3739b9de8f0bcae01ce
Sal Tamay <sal.tamay@outlook.com>
2019-11-28 12:41:10 -0500

    Add functions chapter exercises

---
+++ functions-exercises/exercise-06.js
@@ -0,0 +1,9 @@
+// What does the following code log to the console?
+
+function scream(words) {
+  return words + '!!!!';
+}
+
+scream('Yipeee');
+
+// Logs 'Yipeee!!!!. It takes words argument as a string, then appends '!!!!' to it and returns the result
