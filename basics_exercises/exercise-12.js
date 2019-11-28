6b90febb92fd55907dd1c1a116fef93d5df50092
Sal Tamay <sal.tamay@outlook.com>
2019-11-27 21:58:46 -0500

    Basics exercise-12

---
+++ basics_exercises/exercise-12.js
@@ -0,0 +1,7 @@
+// What value does the following expression evaluate to?
+
+parseInt('3.1415'); // Evaluates to 3. 
+
+// eturns a number when the first character of the string value is a digit or the first two characters are a sign (+ or -) and a digit. 
+// It stops converting and ignores everything else once it encounters an invalid character
+
