4fb0ad625cd9425a49f3b0e8776e182accc71de3
Sal Tamay <sal.tamay@outlook.com>
2019-11-27 21:28:47 -0500

    Basics exercise-04

---
+++ basics_exercises/exercise-04.js
@@ -0,0 +1,7 @@
+// Expression: Any unit of code that can be evaluated to a value is an expression
+// Statement: A statement is an instruction to perform a specific action.
+
+var foo = 5; // Assignment expression
+foo; // In this example, foo is an expression as it evaluates to the value held by foo but it can also pass off as a valid statement.
+
+var foo; // Declaration statement
