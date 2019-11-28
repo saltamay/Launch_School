343f2efee6076b9c24916800bf1356dbd9b5a54d
Sal Tamay <sal.tamay@outlook.com>
2019-11-27 20:25:29 -0500

    Preparations exercise-04

---
+++ preparations_exercises/foo.js
@@ -0,0 +1,17 @@
+var foo = 'bar';
+console.log(foo);
+foo;
+
+// node foo.js 
+// prints only bar
+
+/**
+ * node REPL
+ * > var foo = 'bar';
+ * undefined
+ * > console.log(foo);
+ * bar
+ * undefined
+ * > foo;
+ * 'bar'
+ */
