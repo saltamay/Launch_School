51d983ade2e1978cf90ee09862b8f687df41c53c
Sal Tamay <sal.tamay@outlook.com>
2019-11-27 22:04:01 -0500

    Basics exercise-13

---
+++ basics_exercises/exercise-13.js
@@ -0,0 +1,6 @@
+// What value does the following expression evaluate to?
+
+console.log('12' < '9')
+
+// true. Evaluates through the char value of the first value and the following. Since 9 is bigger than 1
+// the expression evaluates to true
