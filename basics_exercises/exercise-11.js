f1f0b7b1e062179bada56fc121bc60d048015d9c
Sal Tamay <sal.tamay@outlook.com>
2019-11-27 21:54:20 -0500

    Basics exercise-11

---
+++ basics_exercises/exercise-11.js
@@ -0,0 +1,5 @@
+// What value does the following expression evaluate to?
+
+'foo' === 'Foo'
+
+// Evaluates to false beause strict comparison (e.g., ===) is only true if the operands are of the same type and the contents match.
